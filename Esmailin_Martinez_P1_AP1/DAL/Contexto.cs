a62223b3106735930852f001f59ad18c7929ee36
ESMAILINM <esmailinmartinez05@gmail.com>
2025-02-03 19:24:57 -0400

    Crea el Contexto

---
+++ Esmailin_Martinez_P1_AP1/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Esmailin_Martinez_P1_AP1.Models;
+
+namespace Esmailin_Martinez_P1_AP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Modelo> Modelo { get; set; }
+    }
+}
