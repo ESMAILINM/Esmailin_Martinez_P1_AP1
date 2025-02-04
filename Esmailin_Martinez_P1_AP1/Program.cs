95de0d18dc5ad91ccede01ca9c333e2c6dd2f825
ESMAILINM <esmailinmartinez05@gmail.com>
2025-02-03 20:40:00 -0400

    agregar el contexto

--- Esmailin_Martinez_P1_AP1/Program.cs
+++ Esmailin_Martinez_P1_AP1/Program.cs
@@ -1,10 +1,27 @@
 using Esmailin_Martinez_P1_AP1.Components;
+using Esmailin_Martinez_P1_AP1.DAL;
+using Esmailin_Martinez_P1_AP1.Services;
+using Microsoft.EntityFrameworkCore;
+using Esmailin_Martinez_P1_AP1.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+//obtenemos el ConStr para usarlocon el contexto
+var ConSrt = builder.Configuration.GetConnectionString("SqlConStr");
+
+//Agregar contexto al builder con ConStr
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConSrt));
+
+//Inyectar del service
+builder.Services.AddScoped<Servicio>();
+
+
 
 var app = builder.Build();
 
